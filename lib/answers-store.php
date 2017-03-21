5503fa09285845165aa6a8b0b4acf5d60afdac71
t-sauer <t-sauer@outlook.de>
2017-03-21 22:29:06 +0100

    🔧 Fixed PHPCS problems

--- lib/answers-store.php
+++ lib/answers-store.php
@@ -1,29 +1,36 @@
 <?php
 
-class AnswersStore {
+namespace hastdueier;
+
+class AnswersStore
+{
 
     private static $instance;
 
-    public static function instance() {
-      if (!isset(static::$instance)) {
-        static::$instance = new AnswersStore();
-      }
+    public static function instance()
+    {
+        if (!isset(static::$instance)) {
+            static::$instance = new AnswersStore();
+        }
 
-      return static::$instance;
+        return static::$instance;
     }
 
-    private static function loadAnswers() {
-      $string = file_get_contents(dirname(dirname(__FILE__)) . '/data/answers.json');
-      return json_decode($string, true);
+    private static function loadAnswers()
+    {
+        $string = file_get_contents(dirname(dirname(__FILE__)) . '/data/answers.json');
+        return json_decode($string, true);
     }
 
     private $answers;
 
-    public function __construct() {
-      $this->answers = static::loadAnswers();
+    public function __construct()
+    {
+        $this->answers = static::loadAnswers();
     }
 
-    public function random() {
-      return $this->answers[array_rand($this->answers)];
+    public function random()
+    {
+        return $this->answers[array_rand($this->answers)];
     }
 }
